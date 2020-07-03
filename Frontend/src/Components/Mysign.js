18478f1434fc79f619528fe35343f6e82838fa3a
Amdava <amc1@students.uonbi.ac.ke>
2020-07-03 18:40:49 +0300

    Final changes

--- Frontend/src/Components/Mysign.js
+++ Frontend/src/Components/Mysign.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import axios from 'axios';
-import './signin.css';
+import './SignIn.css';
 import { Link } from 'react-router-dom';
 import { Button, Col, Row} from 'reactstrap';
 
